125ec8af9a1a1aaa64d23e42258a8da16c218c7e
LandinBT <daniel.basulto0859@alumnos.udg.mx>
2023-11-01 14:19:32 -0600

    Definition and implementation of the Date class.

--- src/com/landinbt/pharmacy/module/Date.java
+++ src/com/landinbt/pharmacy/module/Date.java
@@ -1,5 +1,7 @@
 package com.landinbt.pharmacy.module;
 
+import java.time.LocalDate;
+
 public class Date {
 	//Attributes
 	private byte day;
@@ -10,31 +12,80 @@ public class Date {
 		return year*10000 + month*100 + day; //20231031
 	}
 	
-	//private boolean isValid(byte d, byte m, int y) {}
+	private boolean isValid(byte d, byte m, int y) {
+		if(d < 1 || d > 31) {
+			return false;
+		}
+		
+		if(m < 1 || m  > 12) {
+			return false;
+		}
+		
+		if(y < 1900 || y > 3000) {
+			return false;
+		}
+		
+		//Check for February
+		if(m == 2) {
+			//Check if year is a leap year
+			if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+				//February has 29 days in a leap year
+				if(d > 29) {
+					return false;
+				}
+			}
+			else {
+				//February has 28 days in a non-leap year
+				if(d > 28) {
+					return false;
+				}
+			}
+		}
+		
+		//Check for months with only 30 days
+		if(m == 4 || m == 6 || m == 9 || m == 11) {
+			if(d > 30) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 	
 	//Methods
-	Date(){}
+	public Date() {
+		LocalDate now = LocalDate.now();
+		this.day = (byte) now.getDayOfMonth();
+		this.month = (byte) now.getMonthValue();
+		this.year = (int) now.getYear();
+	}
 	
-	Date(Date d) {
+	public Date(Date d) {
 		this.day = d.day;
 		this.month = d.month;
 		this.year = d.year;
 	}
 	
 	public void setDay(byte d) {
-		this.day = d;
+		if(isValid(d, this.month, this.year)) {
+			this.day = d;
+		}
 	}
 	
 	public void setMonth(byte m) {
-		this.month = m;
+		if(isValid(this.day, m, this.year)) {
+			this.month = m;
+		}
 	}
 	
 	public void setYear(int y) {
-		this.year = y;
+		if(isValid(this.day, this.month, y)) {
+			this.year = y;
+		}
 	}
 	
 	public String toString() {
-		return day+"/"+month+"/"+year;
+		return String.format("%02d/%02d/%04d", day, month, year);
 	}
 	
 	public byte getDay() {
@@ -48,4 +99,30 @@ public class Date {
 	public int getYear() {
 		return year;
 	}
+	
+	//Relational
+	public boolean equals(Date d) {
+		return toInt() == d.toInt(); // ==
+	}
+		
+	public boolean notEquals(Date d) {
+		return !this.equals(d); // !=
+	}
+		
+	public boolean isLessThan(Date d) {
+		return toInt() < d.toInt(); // <
+	}
+		
+	public boolean isLessThanOrEqual(Date d) {
+		return isLessThan(d) || equals(d); // <=
+	}
+		
+	public boolean isGreaterThan(Date d) {
+		return !isLessThanOrEqual(d); // >
+	}
+		
+	public boolean isGreaterThanOrEqual(Date d) {
+		return !isLessThan(d); // >=
+	}
+		
 }
